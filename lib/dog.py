2badfd4a85aaa26cd8a67db97f898b8ab59bbebe
Justin Bigishiro <justin.rvd@gmail.com>
2023-08-22 20:47:00 -0500

    finish lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,34 @@
 from models import Dog
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-def create_table(base):
-    pass
+def create_table(Base,engine):
+    engine=create_engine('sqlite:///dogs.db')
+    Base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    dogs=session.query(Dog)
+    return [dog for dog in dogs]
 
 def find_by_name(session, name):
-    pass
+    dogs_by_name = session.query(Dog).filter(Dog.name == name).first()
+    return dogs_by_name
+
 
 def find_by_id(session, id):
-    pass
+    dogs_by_id = session.query(Dog).filter(Dog.id == id).first()
+    return dogs_by_id
+
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dogs_by_name_and_breed = session.query(Dog).filter(Dog.name == name , Dog.breed == breed).first()
+    return dogs_by_name_and_breed
+
 
 def update_breed(session, dog, breed):
-    pass
+    for dog in session.query(Dog):
+        dog.breed = breed
